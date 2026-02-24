c483fcd67f42594707697f0a798030cdaf269094
elioraaa <elioraqendro2021@gmail.com>
2026-02-24 18:52:04 +0100

    navbar setup

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,5 +1,29 @@
+import logo from '../../img/logo.png';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
 export default function Navbar() {
-  return (
-    <div className="logo"></div>
-  );
+    return (
+        <>
+    <div className="logo-img">
+        <img src={logo} alt="Vite logo" />
+    </div>
+
+    <Tabs
+      defaultActiveKey="profile"
+      id="uncontrolled-tab-example"
+      className="mb-3"
+    >
+      <Tab eventKey="home" title="Home">
+        Apartaments
+      </Tab>
+      <Tab eventKey="profile" title="Profile">
+        About us
+      </Tab>
+      <Tab eventKey="contact" title="Contact">
+        Contact
+      </Tab>
+    </Tabs>
+    </>
+    );
 }
